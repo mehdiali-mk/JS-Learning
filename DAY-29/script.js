1915f5a19e4b63aaa081d56029eb89c50878347a
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-02-13 17:28:25 +0530

    DAY 29 - Folder is added

---
+++ DAY-29/script.js
@@ -0,0 +1,21 @@
+const inputEl = document.querySelector("#text-input");
+const buttonEl = document.querySelector(".button");
+const resultEl = document.querySelector(".result");
+
+buttonEl.addEventListener("click", (evt) => {
+  evt.preventDefault();
+
+  let value = inputEl.value.toString().toUpperCase();
+  let palindromeValue = value
+    .split("")
+    .reverse()
+    .toString()
+    .toUpperCase()
+    .replaceAll(",", "");
+
+  if (value == palindromeValue) {
+    resultEl.textContent = `${value} is a Palindrome word.`;
+  } else {
+    resultEl.textContent = `${value} is not a Palindrome word.`;
+  }
+});
